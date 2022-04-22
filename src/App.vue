b6132f763016a9bcec21082e2ecbdebba6d1d96b
suxrobshukurov <sdead96@gmail.com>
2022-04-22 17:57:21 +0300

    fetchUser

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,63 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="user">
+    <button type="button" @click="getUser">Получить пользователя</button>
+    <button type="button" @click="getRandomBeer">Получить пиво</button>
+    <p>{{ user.first_name }}</p>
+    <p>{{ user.avatar }}</p>
+    <p>{{ dateOfBirth }}</p>
+    <p>{{ userEmployment }}</p>
+    <p>{{ userAge }}</p>
+
+
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import axios from 'axios';
+import moment from 'moment';
+export default {
+  name: "App",
+  data() {
+    return {
+      user: [],
+      dateOfBirth: '',
+      userAge: '',
+      beer: {},
+      userEmployment: '',
     }
+  },
+  methods: {
+    getUserAge(date_of_birth) {
+      return moment().diff(date_of_birth, 'years', false);
+    },
+    async getUser() {
+      try {
+        const response = await axios.get('https://random-data-api.com/api/users/random_user');
+        this.user = response.data;
+        this.dateOfBirth = this.user.date_of_birth;
+        this.userEmployment = this.user.employment.title
+        console.log(response.data);
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async getRandomBeer() {
+      try {
+        const response = await axios.get('https://random-data-api.com/api/beer/random_beer');
+        this.beer = response.data;
+        console.log(response.data);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  mounted() {
+    this.getUser();
+    this.getUserAge(this.user.date_of_birth);
   }
 }
+</script>
+
+<style scoped>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from "@/App";
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+app.mount('#app')
